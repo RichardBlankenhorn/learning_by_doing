9a0bbdccd54bbba407cb608d71debc9fe0492653
Richard Blankenhorn <rsbnq6@yahoo.com>
2016-02-28 17:42:54 -0700

    Modifying bottles_of_beer.rb sentence structure/wording.
    [#114630921]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,27 +1,31 @@
 #!/usr/bin/env ruby
 
 def bottles_of_beer(beer_quantity)
-  if beer_quantity > 0
-    puts "#{beer_quantity.to_s} bottles of beer on the wall, #{beer_quantity.to_s} bottles of beer.",
-         "You take one down you pass it around, #{(beer_quantity - 1).to_s} bottles of beer on the wall.",
-         ""
+  if beer_quantity > 2
+    puts "#{beer_quantity.to_s} bottles of beer on the wall, #{beer_quantity.to_s} bottles of beer.\n" +
+         "Take one down and pass it around, #{(beer_quantity - 1).to_s} bottles of beer on the wall.\n" +
+         "\n"
     beer_quantity -= 1
   end
 
-  if beer_quantity > 1
+  if beer_quantity > 2
+    bottles_of_beer(beer_quantity)
+  elsif beer_quantity == 2
+    puts "#{beer_quantity.to_s} bottles of beer on the wall, #{beer_quantity.to_s} bottles of beer\n" +
+         "Take one down and pass it around, #{beer_quantity - 1} bottle of beer on the wall.\n" +
+         "\n"
+    beer_quantity -= 1
     bottles_of_beer(beer_quantity)
   elsif beer_quantity == 1
-    puts "#{beer_quantity.to_s} bottle of beer on the wall, #{beer_quantity.to_s} bottle of beer.",
-         "You take one down you pass it around, #{(beer_quantity - 1).to_s} bottles of beer on the wall.",
-         ""
+    puts "#{beer_quantity.to_s} bottle of beer on the wall, #{beer_quantity.to_s} bottle of beer.\n" +
+         "Take one down and pass it around, no more bottles of beer on the wall.\n" +
+         "\n"
     beer_quantity -= 1
     bottles_of_beer(beer_quantity)
   elsif beer_quantity == 0
-    puts "No more bottles of beer on the wall, no more bottles of beer",
-         "Go to the store and buy some more, 99 bottles of beer on the wall"
+    puts "No more bottles of beer on the wall, no more bottles of beer\n" +
+         "Go to the store and buy some more, 99 bottles of beer on the wall\n"
   end
 end
 
 bottles_of_beer(99)
-
-
