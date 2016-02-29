36041c5f62afd17f8ea7ffdd2c746e98d5d28cfc
Richard Blankenhorn <rsbnq6@yahoo.com>
2016-02-29 10:12:43 -0700

    Modifying array_practice.rb to include period at the end of the second to last line
    [#114653231]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -13,5 +13,5 @@ print "The first four elements are '#{(practice_array[0..3]).to_s.gsub('[', '').
 print "If we delete 5, 6 and 7 from the array, we're left with [#{(practice_array - practice_array.slice!(4..6)).join(',')}]\n"
 print "If we add 5 at the beginning of the array, we're left with [#{practice_array.insert(0, 5).join(',')}]\n"
 print "If we add 6 at the end of the array, we're left with [#{practice_array.push(6).join(',')}]\n"
-print "Only the elements #{practice_array.reject! {|element| element < 9}} are > 8\n"
+print "Only the elements #{practice_array.reject! {|element| element < 9}} are > 8.\n"
 print "If we remove all the elements, then the length of the array is #{practice_array.clear.length}\n"
