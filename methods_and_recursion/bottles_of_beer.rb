33c9df0fcef4ac101250112bbdcd3d3f13e63bc2
Richard Blankenhorn <rsbnq6@yahoo.com>
2016-03-08 10:59:43 -0700

    Modifying bottles_of_beer.rb to fix RuboCop errors on length of methods and string concatenation.
    [#114630921]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,31 +1,33 @@
 #!/usr/bin/env ruby
 
+def beer_string(beer_quantity, bottles = 'bottles', bottle = 'bottles')
+  if beer_quantity > 0
+    puts "#{beer_quantity} #{bottles} of beer on the wall,"\
+    " #{beer_quantity} #{bottles} of beer.\n"\
+    "Take one down and pass it around, #{beer_quantity - 1} #{bottle}"\
+    " of beer on the wall.\n"\
+    "\n"
+  else
+    no_beer_string
+  end
+end
+
+def no_beer_string
+  puts "No more bottles of beer on the wall, no more bottles of beer.\n"\
+       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"\
+       "\n"
+end
+
 def bottles_of_beer(beer_quantity)
   if beer_quantity > 2
-    puts "#{beer_quantity.to_s} bottles of beer on the wall, #{beer_quantity.to_s} bottles of beer.\n" +
-         "Take one down and pass it around, #{(beer_quantity - 1).to_s} bottles of beer on the wall.\n" +
-         "\n"
-    beer_quantity -= 1
-  end
-
-  if beer_quantity > 2
-    bottles_of_beer(beer_quantity)
+    beer_string(beer_quantity)
+    bottles_of_beer(beer_quantity - 1)
   elsif beer_quantity == 2
-    puts "#{beer_quantity.to_s} bottles of beer on the wall, #{beer_quantity.to_s} bottles of beer.\n" +
-         "Take one down and pass it around, #{(beer_quantity - 1).to_s} bottle of beer on the wall.\n" +
-         "\n"
-    beer_quantity -= 1
-    bottles_of_beer(beer_quantity)
+    beer_string(beer_quantity, 'bottles', 'bottle')
+    bottles_of_beer(beer_quantity - 1)
   elsif beer_quantity == 1
-    puts "#{beer_quantity.to_s} bottle of beer on the wall, #{beer_quantity.to_s} bottle of beer.\n" +
-         "Take one down and pass it around, no more bottles of beer on the wall.\n" +
-         "\n"
-    beer_quantity -= 1
-    bottles_of_beer(beer_quantity)
-  elsif beer_quantity == 0
-    puts "No more bottles of beer on the wall, no more bottles of beer.\n" +
-         "Go to the store and buy some more, 99 bottles of beer on the wall.\n" +
-         "\n"
+    beer_string(beer_quantity, 'bottle', 'bottles')
+    beer_string(beer_quantity - 1)
   end
 end
 
