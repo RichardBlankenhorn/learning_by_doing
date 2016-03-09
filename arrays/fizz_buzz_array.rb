6d805b0d4c723499c664e155df5d3ff6667d2ae3
Richard Blankenhorn <rsbnq6@yahoo.com>
2016-03-08 23:05:49 -0700

    Modifying fizz_buzz_array.rb to fix RuboCop errors and put just one count += 1 at the end of the 'if' block
    [#114697463]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,23 +1,20 @@
 #!/usr/bin/env ruby
 
 numbers = (1..100).to_a
-fizz_buzz_array = Array.new
+fizz_buzz_array = []
 
 count = 1
 numbers.each do |value|
   if value % 3 == 0 && count % 5 == 0
     fizz_buzz_array.push('FizzBuzz')
-    count += 1
   elsif value % 3 == 0
     fizz_buzz_array.push('Fizz')
-    count += 1
   elsif value % 5 == 0
     fizz_buzz_array.push('Buzz')
-    count += 1
   else
     fizz_buzz_array.push(count)
-    count += 1
   end
+  count += 1
 end
 
-puts fizz_buzz_array
+print fizz_buzz_array
